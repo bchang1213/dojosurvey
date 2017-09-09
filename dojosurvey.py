2f574bc67c8cb88239177c6cd0f12bd9ff48b1a7
Brian Chang <kyoukamu@gmail.com>
2017-09-09 15:35:30 -0700

    created the html form necessary to take in information. Next part will be to use the flask post request to send that submitted data into another page that will display the submitted data.

---
+++ dojosurvey.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request, session
+
+app = Flask(__name__)
+app.secret_key = 'secret103048580e8w7'
+
+# THE DEFAULT LANDING PAGE
+@app.route("/")
+def landing():
+		return render_template("index.html")
+
+@app.route("/result", methods = ['POST'])
+def posting():
+	request.form['new_name']
+	request.form['cars']
+	request.form['cars1']
+	request.form['comment']
+	return render_template("submitted.html")
+
+app.run(debug=True)

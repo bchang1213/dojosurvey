53992642cd323f6341b4e9a61f5a5f6810edb9d8
Brian Chang <kyoukamu@gmail.com>
2017-09-11 12:19:20 -0700

    had to make a change/debug the comment area, where the comment had to equal a value or an empty string.

--- dojosurvey.py
+++ dojosurvey.py
@@ -6,10 +6,14 @@ app.secret_key = 'secret103048580e8w7'
 # THE DEFAULT LANDING PAGE
 @app.route("/")
 def landing():
-		return render_template("index.html")
+	return render_template("index.html")
 
-@app.route("/result", methods = ['POST'])
+@app.route("/result", methods = ["POST"])
 def posting():
-	return render_template("submitted.html",name = request.form['new_name'], cars = request.form['cars'], cars1 = request.form['cars1'], comment = request.form['comment'])
+	name = request.form['new_name']
+	cars = request.form['cars']
+	cars1 = request.form['cars1']
+	comment = request.form['comment'] or " "
+	return render_template("submitted.html", name = name, cars = cars, cars1 = cars1, comment = comment)
 
 app.run(debug=True)

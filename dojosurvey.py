680e6302921f1e0c2ffb95df3f0bf28915849cdc
Brian Chang <kyoukamu@gmail.com>
2017-09-12 16:57:14 -0700

    added name and comment validation

--- dojosurvey.py
+++ dojosurvey.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash, session
 
 app = Flask(__name__)
 app.secret_key = 'secret103048580e8w7'
@@ -10,10 +10,31 @@ def landing():
 
 @app.route("/result", methods = ["POST"])
 def posting():
-	name = request.form['new_name']
-	cars = request.form['cars']
-	cars1 = request.form['cars1']
-	comment = request.form['comment'] or ""
-	return render_template("submitted.html", name = name, cars = cars, cars1 = cars1, comment = comment)
+	if len(request.form['new_name']) < 1:
+		flash("Name cannot be empty!") # just pass a string to the flash function
+		return redirect("/")
+	else:
+		session['name'] = request.form['new_name']
+
+	session['cars'] = request.form['cars']
+	session['cars1'] = request.form['cars1']
+
+	if len(request.form['comment']) < 1:
+		flash("Comment cannot be empty!")
+		return redirect("/")
+	else:
+		session['comment'] = request.form['comment']
+
+	return redirect('/submitted')
+
+@app.route("/submitted")
+def submitted():
+	return render_template(
+		"submitted.html",
+		name = session['name'],
+		cars = session['cars'],
+		cars1 = session['cars1'],
+		comment = session['comment']
+		)
 
 app.run(debug=True)
